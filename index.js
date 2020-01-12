303bd9437666d63409143586e1c9c27d85447ea7
Giovani Milani <giovanimilani11@gmail.com>
2020-01-11 22:55:43 -0300

    Resolve for product and calc for discount

--- index.js
+++ index.js
@@ -3,27 +3,43 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
   scalar Date
 
-  type Usuario {
-    id: ID
-    nome: String!
-    email: String!
-    idade: Int
-    salario: Float
-    vip: Boolean
-  }
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
 
-  # Pontos de entrada da sua API!
-  type Query {
-    ola: String!
-    horaCerta: Date!
-    usuarioLogado: Usuario
-  }
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float!
+    }
+
+    # Pontos de entrada da sua API!
+    type Query {
+        ola: String!
+        horaCerta: Date!
+        usuarioLogado: Usuario
+        produto: Produto
+    }
 `
 
 const resolvers = {
     Usuario: {
         salario(usuario) {
             return usuario.salario_real
+        }
+    },
+    Produto: {
+        precoComDesconto(produto) {
+            if (produto.desconto) {
+                return produto.preco * (1 - produto.desconto)
+            }
+            return produto.preco
         }
     },
     Query: {
@@ -41,6 +57,13 @@ const resolvers = {
                 idade: 23,
                 salario_real: 1234.56,
                 vip: true
+            }
+        },
+        produto() {
+            return {
+                nome: "Produto muito barato",
+                preco: 10,
+                desconto: 0.15
             }
         }
     }
