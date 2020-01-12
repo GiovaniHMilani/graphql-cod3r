76f3ec1206f9941d3b3599453e4cc6d415d8e090
Giovani Milani <giovanimilani11@gmail.com>
2020-01-11 22:39:17 -0300

    Resolver for User

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
+  scalar Date
+
+  type Usuario {
+    id: ID
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+
   # Pontos de entrada da sua API!
   type Query {
     ola: String!
-    horaCerta: String!
+    horaCerta: Date!
+    usuarioLogado: Usuario
   }
 `
 
 const resolvers = {
-  Query: {
-    ola() {
-      return 'Bom dia!'
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        }
     },
-    horaCerta() {
-      return `${new Date()}`;
+    Query: {
+        ola() {
+            return 'Bom dia!'
+        },
+        horaCerta() {
+            return new Date;
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: "Ana da web",
+                email: "anadaweb@gmail.com",
+                idade: 23,
+                salario_real: 1234.56,
+                vip: true
+            }
+        }
     }
-  }
 }
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+    typeDefs,
+    resolvers
 })
 
 server.listen().then(({ url }) => {
-  console.log(`Executando em ${url}`);
+    console.log(`Executando em ${url}`);
 })
