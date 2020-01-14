919f792dcc3b200e04bf2c61c9c21a3ede9dd5ee
Giovani Milani <giovanimilani11@gmail.com>
2020-01-14 08:16:10 -0300

    add query for perfil

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+
+
+const perfis = [{
+    id: 1,
+    nome: 'Comum',
+}, {
+    id: 2,
+    nome: 'Administrador',
+}]
+
+const usuarios = [{
+    id: 1,
+    nome: 'João Silva',
+    email: 'jsilva@zemail.com',
+    idade: 29,
+    perfil_id: 1
+}, {
+    id: 2,
+    nome: 'Rafael Junior',
+    email: 'rafajun@wemail.com',
+    idade: 31,
+    perfil_id: 2
+}, {
+    id: 3,
+    nome: 'Daniela Smith',
+    email: 'danismi@umail.com',
+    idade: 24,
+    perfil_id: 1
+}]
+
 const typeDefs = gql`
-  scalar Date
+    scalar Date
+
+    type Perfil {
+        id: Int!
+        nome: String!
+    }
 
     type Usuario {
-        id: ID
+        id: Int
         nome: String!
         email: String!
         idade: Int
         salario: Float
         vip: Boolean
+        perfil: Perfil
     }
 
     type Produto {
@@ -26,6 +62,10 @@ const typeDefs = gql`
         usuarioLogado: Usuario
         produtoEmDestaque: Produto
         numerosMegaSena: [Int!]!
+        usuarios: [Usuario]
+        usuario(id: Int): Usuario
+        perfis: [Perfil]
+        perfil(id: Int): Perfil
     }
 `
 
@@ -33,6 +73,10 @@ const resolvers = {
     Usuario: {
         salario(usuario) {
             return usuario.salario_real
+        },
+        perfil(usuario) {
+            const selecionados = perfis.filter(p => p.id === usuario.perfil_id)
+            return selecionados ? selecionados[0] : null
         }
     },
     Produto: {
@@ -71,6 +115,20 @@ const resolvers = {
             const crescente = (a, b) => a - b
             return Array(6).fill(0).map(n => parseInt(Math.random() * 60 + 1))
                 .sort(crescente)
+        },
+        usuarios() {
+            return usuarios;
+        },
+        usuario(_, { id }) {
+            const selecionados = usuarios.filter(u => u.id === id)
+            return selecionados ? selecionados[0] : null
+        },
+        perfis() {
+            return perfis;
+        },
+        perfil(_, { id }) {
+            const selecionados = perfis.filter(p => p.id === id)
+            return selecionados ? selecionados[0] : null
         }
     }
 }
